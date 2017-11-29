1599a433e07a2e469abd339479a6ad500a251665
Brian Fischer <starfox139@yahoo.com>
2017-11-29 00:41:09 -0500

    No longer need .rat-component

--- RatWorks.js
+++ RatWorks.js
@@ -15,10 +15,25 @@ RatWorks.appendNewComponent = function (elementName, data, jqParent) {
 	return newJQ;
 }
 
+var customCount = function () {
+	let totalCustomCount = 0;
+	Object.keys(customComponents).forEach(function (elementName) {
+		totalCustomCount += $(elementName).length;
+	});
+	return totalCustomCount;
+}
+
 var scrub = function (jqElement) {
-	while ($('.rat-component').length > 0) {
-		let currentJQElement = $($('.rat-component')[0]);
-		currentJQElement.prop('outerHTML', customComponents[currentJQElement.prop('tagName').toLowerCase()](currentJQElement.data()));
+	let customElementsRemain = true;
+	while (customElementsRemain) {
+		customElementsRemain = false;
+		Object.keys(customComponents).forEach(function (elementName) {
+			while ($(elementName).length > 0) {
+				customElementsRemain = true;
+				let currentJQElement = $($(elementName)[0]);
+				currentJQElement.prop('outerHTML', customComponents[currentJQElement.prop('tagName').toLowerCase()](currentJQElement.data()));
+			}
+		});
 	}
 }
 
